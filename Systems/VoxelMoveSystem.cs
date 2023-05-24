d1176ac3fee92ce03cbd564b04e5ef00ac12eb64
Michael Ott <mott@microsoft.com>
2023-05-23 19:05:15 -0700

    Simplify movement logic to eliminate continue.

--- Systems/VoxelMoveSystem.cs
+++ Systems/VoxelMoveSystem.cs
@@ -45,54 +45,51 @@ namespace SandSimulator.Systems
 				var moveComponent = _movingVoxelMapper.Get(entityId);
 
 				var oldPos = posComponent.Position;
+				var oldDirection = moveComponent.Direction;
+
+				var newPos = Material.PotentialMove(_grid, oldPos);
 
 				// Addresses corner case where swaping pixels on a successful move can move it out from underneath an active move.
-				if (_grid[oldPos] == moveComponent.SourceVoxel)
+				if (_grid[oldPos] == moveComponent.SourceVoxel && newPos != null)
 				{
-					var oldDirection = moveComponent.Direction;
+					var newDirection = newPos.Value - oldPos;
 
-					var newPos = Material.PotentialMove(_grid, oldPos);
-					if (newPos != null)
+					// If the last velocity is a valid lateral move, use it
+					if (oldDirection.Y == 0 && newDirection.Y == 0 && Material.IsValidMove(_grid, oldPos, oldPos + oldDirection))
 					{
-						var newDirection = newPos.Value - oldPos;
+						newPos = oldPos + oldDirection;
+						newDirection = oldDirection;
+					}
 
-						// If the last velocity is a valid lateral move, use it
-						if (oldDirection.Y == 0 && newDirection.Y == 0 && Material.IsValidMove(_grid, oldPos, oldPos + oldDirection))
+					// Update direction
+					moveComponent.Direction = newDirection;
+					_movingVoxelMapper.Put(entityId, moveComponent);
+
+					// Update position
+					occupiedPositions.Add(newPos.Value);
+					_grid.Swap(oldPos, newPos.Value);
+					posComponent.Position = newPos.Value;
+					_posMapper.Put(entityId, posComponent);
+
+					// Check all positions around this one to see if they may be impacted
+					// Including the new destination position
+					foreach (var offset in Offsets)
+					{
+						var testPos = new IntVector2 { X = oldPos.X + offset.X, Y = oldPos.Y + offset.Y };
+						if (_grid[testPos] != VoxelType.None)
 						{
-							newPos = oldPos + oldDirection;
-							newDirection = oldDirection;
+							potentialCheckPositions.Add(testPos);
 						}
-
-						// Update direction
-						moveComponent.Direction = newDirection;
-						_movingVoxelMapper.Put(entityId, moveComponent);
-
-						// Update position
-						occupiedPositions.Add(newPos.Value);
-						_grid.Swap(oldPos, newPos.Value);
-						posComponent.Position = newPos.Value;
-						_posMapper.Put(entityId, posComponent);
-
-						// Check all positions around this one to see if they may be impacted
-						// Including the new destination position
-						foreach (var offset in Offsets)
-						{
-							var testPos = new IntVector2 { X = oldPos.X + offset.X, Y = oldPos.Y + offset.Y };
-							if (_grid[testPos] != VoxelType.None)
-							{
-								potentialCheckPositions.Add(testPos);
-							}
-						}
-
-						continue;
 					}
 				}
-
-				// Move is no longer valid, remove it.
-				var entity = GetEntity(entityId);
-				entity.Detach<PositionComponent>();
-				entity.Detach<MovingVoxelComponent>();
-				entity.Destroy();
+				else
+				{
+					// Move is no longer valid, remove it.
+					var entity = GetEntity(entityId);
+					entity.Detach<PositionComponent>();
+					entity.Detach<MovingVoxelComponent>();
+					entity.Destroy();
+				}
 			}
 
 			foreach (var pos in potentialCheckPositions)
